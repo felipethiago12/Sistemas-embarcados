fa121240bef99cbc6d0b928d2f59acd9a834c354
Felipe Thiago <30877074+felipethiago12@users.noreply.github.com>
2017-09-15 17:18:22 -0300

    Add files via upload

---
+++ 1_respostas/Aula03/2_olausuario/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   char nome[50];
+ 
+    printf("Qual seu nome: ");
+    scanf("%s",nome);
+ 
+    printf("\n Olá %s!\n", nome);
+
+    return 0;
+}      
---
+++ 1_respostas/Aula03/8_numstring/num_caracs.c
@@ -0,0 +1,10 @@
+#include "num_caracs.h"
+
+int num_caracs(char *string){
+
+	int i=0;
+	while(string[i] =! '\0'){
+		i++;
+	}
+	return i;
+}
